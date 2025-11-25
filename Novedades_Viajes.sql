136e65c8fc32f32c57d84fc6c1655fe8120292ff
MaRob1321 <98710691+MaRob1321@users.noreply.github.com>
2025-11-25 06:21:40 -0300

    Delete Novedades_Viajes.sql

--- Novedades_Viajes.sql
+++
@@ -1,5 +0,0 @@
-SELECT Remitente, Asunto, Dominio, Viaje, Fecha_Hora_Actualizacion
-FROM DW.Novedades_Viajes
-WHERE CAST(fecha_Sys AS DATE) IN (
-    CAST(GETDATE() AS DATE),
-    CAST(DATEADD(DAY, -1, GETDATE()) AS DATE))
